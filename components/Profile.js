23dc941d6005c6d0af1baf90bff903cb5e282b28
juan.munar2499 <juan.munar2499@gmail.com>
2025-01-15 18:49:02 +0000

    instalacion de iconos font awesome y modificacion de pagina de perfil para que se vea mejor y tenga una mejor organizacion./home/user/portafolio/components/Profile.js

--- components/Profile.js
+++ components/Profile.js
@@ -1,35 +1,46 @@
 import React from 'react';
 import Image from 'next/image';
-import styles from './Profile.module.css'; // Importa los estilos
+import styles from './Profile.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faPhone, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Profile = () => {
-  return (
-    <section className={styles.profile}> {/* Usa className con CSS Modules */}
-      <div className={styles['profile-content']}>
-        <div className={styles['profile-image']}>
-          <Image
-            src="/images.jpg" // Ruta a tu imagen de perfil (en public/images)
-            alt="Foto de Perfil"
-            width={200}
-            height={200}
-            className={styles.profileImage} // Aplica estilos a la imagen
-          />
-        </div>
-        <div className={styles['profile-info']}>
-          <h1>Juan Sebastian Munar Diaz</h1>
-          <h2>Tecnico en Mantenimieto de Equipos de Computo</h2>
-          <p>
-            Motivado por mi pasión por la tecnología, estoy constantemente explorando nuevas herramientas y conocimientos. Mi experiencia en hardware y software, combinada con mi proactividad, me permiten enfrentar desafíos técnicos y encontrar soluciones creativas. Aspiro a convertirme en un referente en el campo y contribuir al desarrollo de proyectos innovadores.
-          </p>
-          <div className={styles['profile-contact']}>
-            <p>Email: jsmd2499@outlook.com</p>
-            <p>Teléfono: +57 3014310093</p>
-            <p>Ubicación: Bogotá, Colombia</p>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.profile}>
+            <div className={styles['profile-content']}>
+                <div className={styles['profile-image']}>
+                    <Image
+                        src="/images.jpg"
+                        alt="Foto de Perfil"
+                        width={200}
+                        height={200}
+                        className={styles.profileImage}
+                    />
+                </div>
+                <div className={styles['profile-info']}>
+                    <h1>Juan Sebastian Munar Diaz</h1>
+                    <h2>Técnico en Mantenimiento de Equipos de Computo</h2>
+                    <p>
+                        Motivado por mi pasión por la tecnología, estoy constantemente explorando nuevas herramientas y conocimientos. Mi experiencia en hardware y software, combinada con mi proactividad, me permiten enfrentar desafíos técnicos y encontrar soluciones creativas. Aspiro a convertirme en un referente en el campo y contribuir al desarrollo de proyectos innovadores.
+                    </p>
+                    <div className={styles['contact-info']}>
+                        <div className={styles['contact-item']}>
+                            <FontAwesomeIcon icon={faEnvelope} className={styles['contact-icon']} />
+                            <a href="mailto:jsmd2499@outlook.com">jsmd2499@outlook.com</a>
+                        </div>
+                        <div className={styles['contact-item']}>
+                            <FontAwesomeIcon icon={faPhone} className={styles['contact-icon']} />
+                            <a href="tel:+573014310093">+57 301 431 0093</a> {/* Formato de número de teléfono */}
+                        </div>
+                        <div className={styles['contact-item']}>
+                            <FontAwesomeIcon icon={faMapMarkerAlt} className={styles['contact-icon']} />
+                            <span>Bogotá, Colombia</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Profile;
